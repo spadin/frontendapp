fba4b62260e146085fbf4db528b3272b9cc3ca1d
Sandro Padin <sandropadin@gmail.com>
2016-06-18 13:40:36 -0500

    Add more coverage to greetingTest.js

--- test/greeter/greetingTest.js
+++ test/greeter/greetingTest.js
@@ -1,6 +1,10 @@
 import test from 'ava';
 import greeting from '../../src/javascript/greeter/greeting';
 
-test('#greeting', (t) => {
+test('returns Hello World! when no name is passed in', (t) => {
+  t.is(greeting(), 'Hello World!');
+});
+
+test('returns Hello Sam! when Sam is passed in', (t) => {
   t.is(greeting('Sam'), 'Hello Sam!');
 });
